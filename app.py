7af7d8f45e332614936bce241512a0b4c2d69222
PrAsAnNaRePo <prasannatwenty@gmail.com>
2024-05-22 16:40:00 +0530

    Edited system prompt

--- app.py
+++ app.py
@@ -9,13 +9,30 @@ st.title("FollowUp'er")
 client = OpenAI()
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-3.5-turbo-0125"
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {
             'role': 'system',
-            'content': "you'll be give a question and the answer for that question by user.  you have to try to make a other new questions based on the topic the they are discussing. Generate a single further questions that continues in the flow of the Q&A. Try this for full conversation. Again, please generate only one follow up question for each conversation. Also make sure that you are returning **only the question**, do not include any other text like 'sure here is the question' or 'further question is below'. Don't include any other contents, just question."
+            'content': """You are a AI chatbot.
+Here are the questions you have to user **majorly**
+- Tell of an instance when you made yourself available to a colleague
+- Tell about the time you demonstrated your ability to actively listen
+- Tell about the time you demonstrated your analytical skills
+
+In between of these question, you ask the follow up question to the user depends on the answer. Ask 2 or 3 follow-up questions for these 3 major question.
+
+## Note:
+- Don't include any other random text/content, just write question.
+- Make follow up question consicse as other major questions.
+- Don't ask the follow-up questions at a time. Ask them seperatly, because you have to frame the questions based on the answer given to the above question.
+- After finish asking the 2 or 3 follow up question, go and ask the next major question and continue in the same way.
+- At last after finishing asking all the major questions and thier follow up questions, finish up Thank you to the user to end conversation."""
+        },
+        {
+            'role': 'assistant',
+            'content': 'Tell of an instance when you made yourself available to a colleague'
         }
     ]
 
